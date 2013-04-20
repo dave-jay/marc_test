ecca87e1be3d353f6caf07cd93b111b647f73ab9
Jay Dave <dave.jay90@gmail.com>
2013-04-21 04:33:46 +0530

    Task 4

--- task4.php
+++ task4.php
@@ -33,10 +33,31 @@ function sanitize($html, $allowed) {
     }
     $allowed_tags = "<" . implode(">,<", $allowed_tags) . ">";
     $return = strip_tags($html, $allowed_tags);
-    
-    
 
-    
+
+    $matches = array();
+    foreach ($allowed as $tag => $attr) {
+        $allowed_attribtues = array();
+        if (is_array($attr)) {
+            foreach ($attr as $ea) {
+                preg_match('/<'.$tag.'\s*(.*?)\s*'.$ea.'=\'(.*?)\'>/i', $return, $matches);
+                // get all matched attributes and save in an array
+                if ($matches[2]) {
+                    $value = substr($matches[2], 0, strpos($matches[2], "'"));
+                    $allowed_attribtues[] = " {$ea} = '{$value}' ";
+                }
+            }
+            
+            // now, replace all attributes with allowed content
+            if (!empty($allowed_attribtues)) {
+                $allowed_attribtues = implode(" ", $allowed_attribtues);
+                $return = preg_replace('/<'.$tag.'\s*(.*?)\s*>/i', "<{$tag} {$allowed_attribtues}>", $return);
+                
+            }
+        }
+    }
+
+    return $return;
 }
 
 $html = "<div><p align='left' onclick='alert(1)'>sample <b><i>text</i></b><script>alert(2);</script></p></div>";
